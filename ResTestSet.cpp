d7d9a268064713ebd4ca58ce2a6979467021a439
Haroldo Gambini Santos <haroldo.santos@gmail.com>
2019-03-18 13:21:02 +0100

    rest test set

--- ResTestSet.cpp
+++ ResTestSet.cpp
@@ -5,9 +5,15 @@
  *      Author: haroldo
  */
 
-#include "ResTestSet.hpp"
 #include <cstdlib>
+#include <algorithm>
 #include <cstdio>
+#include <cfloat>
+#include <cmath>
+#include "ResTestSet.hpp"
+#include "Parameters.hpp"
+
+using namespace std;
 
 ResTestSet::ResTestSet(
     const std::unordered_map<std::string, size_t> &_instances,
@@ -16,7 +22,7 @@ ResTestSet::ResTestSet(
     res_(nullptr)
 {
     FILE *f=fopen( fileName, "r" );
-    char line[4096];
+    char line[4096] = "";
 
     // ignoring headers
     if (!fgets(line, 4096, f))
@@ -30,12 +36,36 @@ ResTestSet::ResTestSet(
     for ( size_t i=1 ; (i<_instances.size()) ; ++i )
         res_[i] = res_[i-1] + _algsettings.size();
 
+    char **loaded = new char*[_instances.size()];
+    loaded[0] = new char[_instances.size()*_algsettings.size()];
+    for ( size_t i=0 ; i<(_instances.size()*_algsettings.size()) ; ++i )
+        loaded[0][i] = false;
+    for ( size_t i=1 ; i<(_instances.size()) ; ++i )
+        loaded[i] = loaded[i-1] + _algsettings.size();
+
+    long double sum = 0.0;
+    size_t nRes = 0;
+    double worseRes = DBL_MIN;
+
+    long double *sumInst = new long double[_instances.size()];
+    for ( size_t i=0 ; (i<_instances.size()) ; ++i )
+        sumInst[i] = 0.0;
+    size_t *nResInst = new size_t[_instances.size()];
+    for ( size_t i=0 ; (i<_instances.size()) ; ++i )
+        nResInst[i] = 0;
+    double *worseInst = new double[_instances.size()];
+    for ( size_t i=0 ; (i<_instances.size()) ; ++i )
+        worseInst[i] = DBL_MIN;
+    double *avgInst = new double[_instances.size()];
+    for ( size_t i=0 ; (i<_instances.size()) ; ++i )
+        avgInst[i] = 1e20;
+
     while (char *s=fgets(line, 4096, f))
     {
-        char instName[256];
-        char algSetting[256];
-        double res;
-        int nr = sscanf(line, "%s,%s,%g", instName, algSetting, res);
+        char instName[256]="";
+        char algSetting[256]="";
+        float res=DBL_MAX;
+        int nr = sscanf(s, "%s,%s,%g", instName, algSetting, &res);
         if (nr!=3)
         {
             fprintf(stderr, "invalid file format\n");
@@ -46,13 +76,63 @@ ResTestSet::ResTestSet(
         if (iti == _instances.end())
             continue;
 
-        auto ita = _algsettings.find(std::string(instName));
+        auto ita = _algsettings.find(std::string(algSetting));
         if (ita == _algsettings.end())
             continue;
 
+        loaded[iti->second][ita->second] = true;
         res_[iti->second][ita->second] = res;
+
+        sum += res;
+        nRes++;
+
+        sumInst[iti->second] += res;
+        nResInst[iti->second]++;
+        worseInst[iti->second] = max( (double)worseInst[iti->second], (double)res);
+        worseRes = max( worseRes, (double)res);
     }
     fclose(f);
+
+    for ( size_t i=0 ; (i<_instances.size()) ; ++i )
+        if (nResInst[i])
+            avgInst[i] = ((long double)sumInst[i]) / ((long double)nResInst[i] );
+        else
+            avgInst[i] = worseRes;
+
+    for ( size_t i=0 ; (i<_instances.size()) ; ++i )
+    {
+        for ( size_t j=0 ; (j<_algsettings.size()) ; ++j )
+        {
+            if (loaded[i][j])
+                continue;
+
+            switch (Parameters::fmrStrategy)
+            {
+                case FMRStrategy::Worse:
+                    res_[i][j] = worseRes;
+                    break;
+                case FMRStrategy::WorseT2:
+                    res_[i][j] = worseRes + fabs(worseRes);
+                    break;
+                case FMRStrategy::WorseInst:
+                    res_[i][j] = worseInst[i];
+                    break;
+                case FMRStrategy::WorseInstT2:
+                    res_[i][j] = worseInst[i]+fabs(worseInst[i]);
+                    break;
+                case FMRStrategy::AverageInst:
+                    res_[i][j] = avgInst[i];
+                    break;
+            }
+        }
+    }
+
+    delete[] avgInst;
+    delete[] worseInst;
+    delete[] loaded[0];
+    delete[] loaded;
+    delete[] sumInst;
+    delete[] nResInst;
 }
 
 float ResTestSet::get( size_t idxInst, size_t idxAlgSetting ) const
