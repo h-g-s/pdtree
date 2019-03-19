e865ea51b130e32a4829e8c94e12361ca5f55373
Haroldo <haroldo.santos@gmail.com>
2019-03-19 15:33:42 +0100

    bench dir

---
+++ bench/bench.sh
@@ -0,0 +1,19 @@
+export CXXFLAGS="-Ofast -flto -DNDEBUG" 
+export LDFLAGS="-Ofast -flto" 
+cd ..
+./configure ; make clean ; make
+rm kfold.csv -f
+mv kfold bench
+cd bench
+
+export K=10
+
+for depth in 1 2 3 4 5;
+do
+    for mlb in 50 100 150 200 250 300;
+    do
+        ./kfold ~/inst/algsel/rcpsp/features.csv ~/inst/algsel/rcpsp/results.csv $K rcpsp -maxDepth=${depth} -minElementsBranch=${mlb}
+        ./kfold ~/inst/algsel/cbcrelax/features.csv ~/inst/algsel/cbcrelax/results.csv $K cbcrelax -maxDepth=${depth} -minElementsBranch=${mlb}
+    done
+done
+
